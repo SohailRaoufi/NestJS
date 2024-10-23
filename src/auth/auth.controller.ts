745ebf5ae63a7978ffceda56dafc7432ed0d97af
Sohail Raoufi <sohailraufi19@gmail.com>
2024-10-23 14:30:01 +0430

    Auth System Added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,9 +7,9 @@ import { AuthDto } from "./dto";
 @Controller('auth')
 export class AuthController{
     constructor(private authservice: AuthService){}
-    @Get("signin")
-    signin(){
-        return this.authservice.signin();
+    @Post("signin")
+    signin(@Body() dto:AuthDto){
+        return this.authservice.signin(dto);
     }
 
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,62 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { error } from "console";
 
 
 @Injectable()
 export class AuthService{
     constructor(private prisma: PrismaService){}
-    signin(){
-        return {msg : "You are Signed in!"};
+    async signin(dto:AuthDto){
+        // find user
+        const user = await this.prisma.user.findUnique({
+            where:{
+                email:dto.email,
+            }
+        })
+        const verify = await argon.verify(user.hash, dto.password);
+
+        if(!user){
+            throw new ForbiddenException("Wrong Crediential!");
+        }
+        if(!verify){
+            throw new ForbiddenException("Crediential Incorrect!");
+        }
+
+        return `Signed In, Welcome ${user.email.split("@")[0]}`;
     }
 
-    signup(dto:AuthDto){
+    async signup(dto:AuthDto){
         // now we have the data and first we must hash the password.
-        return {msg : "You are Signed Up!"};
+
+        // generate Password has
+        const hash = await argon.hash(dto.password);
+
+        // save the new user in db
+        try {
+            const user = await this.prisma.user.create({
+                data:{
+                    email:dto.email,
+                    hash,
+                },
+                select:{ // only returns the seleceted fileds like filter.
+                    id:true,
+                    email:true,
+                    createdAt:true,
+                }
+            })
+            return user;
+        } catch(error){
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code === "P2002"){ //prisma Error code
+                    throw new ForbiddenException("Credntioal Exists!");
+                }
+            }
+
+            throw error;
+        }
+        
     }
 }
